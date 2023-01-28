e39be1194636b9e712278a85a6a05acf9ef66a8b
Gabi <gabrielpk.18@gmail.com>
2023-01-27 22:05:06 -0300

    feat: support devnet v4

--- geth/geth.sh
+++ geth/geth.sh
@@ -4,10 +4,10 @@ geth init  --datadir /db /config/genesis.json
 
 exec geth \
   --datadir /db \
-  --networkid 1331 \
+  --networkid 4844001004 \
   --syncmode=full \
   --verbosity 4 \
   --authrpc.jwtsecret /config/jwtsecret \
   --authrpc.vhosts="*" \
   --authrpc.addr=0.0.0.0 \
-  --bootnodes "enode://fdefa8b36717bd246ce338e952a3b949e608158d06c74c8b9e207439f70bfc63da27b7a4c4161a14722f7d4a256fda9c93a6dd5c26624b6a523dca2cd852ad97@34.121.33.53:30303"
+  --bootnodes "enode://8488a57c323d5e5bef1c40c5cb2330cb0bd75d900afad5e7f50a47ecc1ae7683fb664f509908c6b978633b9af75788e943f26e9b64989504d189ae7b3278ede3@161.35.91.184:30303"
